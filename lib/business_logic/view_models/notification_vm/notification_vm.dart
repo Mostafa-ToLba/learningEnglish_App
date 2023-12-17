7bfc7d13ed1a208c500150106decced8dc0af52a
Rana-Yehya <elrana112@gmail.com>
2023-12-17 21:38:21 +0200

    adding Notification model and view model

---
+++ lib/business_logic/view_models/notification_vm/notification_vm.dart
@@ -0,0 +1,12 @@
+//notification_vm
+
+
+import 'package:learning_anglish_app/business_logic/setup/base_notifier.dart';
+import 'package:learning_anglish_app/models/notification_model.dart';
+
+class NotificationViewModel extends BaseNotifier
+{
+  NotificationModel? notificationModel;
+
+  
+}
